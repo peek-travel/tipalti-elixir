a4029e5ac534f5aa95aaffe2dba2cf1fd3eb88a3
Chris Dosé <chris.dose@gmail.com>
2021-05-19 17:25:59 -0700

    Updates for OTP 24.0

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Tipalti.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :xmerl]
     ]
   end
 
